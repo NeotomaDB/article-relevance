2196134f0918628790f78dd519d66cb2d1f77247
SimonGoring <goring@wisc.edu>
2024-02-13 10:45:12 -0800

    Updating function names in init.

--- src/article_relevance/__init__.py
+++ src/article_relevance/__init__.py
@@ -1,11 +1,13 @@
 #from .logs import get_logger
 from .loadPQ import loadPQ
 from .gddQuery import gddQuery
-from .crossRefQuery import crossRefQuery
-from .dataPreprocessing import dataPreprocessing
-from .addEmbeddings import addEmbeddings
+from .rec_print import rel_print
+from .crossref_query import crossref_query
+from .dataPreprocessing import data_preprocessing
+from .addEmbeddings import add_embeddings
 from .relevancePredict import relevancePredict
 from .relevancePredictTrain import relevancePredictTrain
 from .predToPQ import predToPQ
 from .NeotomaOneHotEncoder import NeotomaOneHotEncoder
 from .updateSource import updateSource
+from .s3_management import pull_s3, push_s3, update_dois
