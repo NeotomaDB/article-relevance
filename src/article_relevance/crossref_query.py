730d00076fb64ed911b76894c513519881fb2c8f
SimonGoring <goring@wisc.edu>
2024-02-26 09:50:54 -0800

    Fixing up the crossref_query so that it uses the file listing & passes warnings
    This now uses boto3 directly (instead of passing it off to the pull_s3), so that we can get the bucket directory listing and then find all the DOI json files.
    This speeds up parsing and reading files since we only need the listings once, rather than recovering each file sequentially.

--- src/article_relevance/crossref_query.py
+++ src/article_relevance/crossref_query.py
@@ -2,9 +2,11 @@ import warnings
 #import re
 from datetime import datetime
 import json
+import boto3
 import requests
 import pandas as pd
 from .s3_management import pull_s3, push_s3
+from botocore.exceptions import ClientError
 from .raw_crossref import raw_crossref
 #from src.article_relevance import rel_print
 #from .logs import get_logger
@@ -30,35 +32,38 @@ def crossref_query(doi_store, metadata_store, verbose = False, check = False, cr
     """
     # First, pull the set of existing DOIs
     doi_df = pull_s3(doi_store)
+    metadata_df = pull_s3(metadata_store)
     # The function calls for DOIs and posts them in the S3 bucket as raw JSON.
     # It is not importing the metadata into a dataframe. That happens later.
     # It returns a list of DOIs that are in the S3 bucket.
-    attempt = raw_crossref(doi_df['doi'], doi_store['Bucket'])
+    attempt = raw_crossref(doi_df['doi'], metadata_store)
     if verbose:
         print(f'Processed {len(attempt)} DOIs of the total {len(doi_df.index)} objects.')
     try:
         metadata = pull_s3(metadata_store)
     except ClientError as ex:
-        # Error is thrown if there is no current file in S3
+        # Error is thrown if there is no metadata file in S3 so we have to create an empty one.
         if ex.response['Error']['Code'] == 'NoSuchKey':
             if create:
                 metadata = pd.DataFrame({'doi': [],
-                                         'title': None,
-                                         'subtitle': None,
-                                         'author': None,
-                                         'subject': None,
-                                         'abstract': None,
-                                         'container-title': None,
-                                         'language': None,
-                                         'published': None,
-                                         'publisher': None,
-                                         'url': None,
-                                         'date': None})
+                                            'title': None,
+                                            'subtitle': None,
+                                            'author': None,
+                                            'subject': None,
+                                            'abstract': None,
+                                            'container-title': None,
+                                            'language': None,
+                                            'published': None,
+                                            'publisher': None,
+                                            'url': None,
+                                            'date': None})
+    # Now we see how many of the DOIs we have in our primary list need to be
+    # added to the metadata object:
+    missing_dois = [i for i in attempt if i not in list(metadata['doi'])]
     if verbose:
         print(f'{len(doi_df.index)} DOIs passed to the function.')
-    checkers = [i for i in attempt if i not in list(metadata['doi'])]
-    if verbose:
-        print(f'{len(checkers)} DOIs to be added to the metadata store.')
+        print(f'{len(missing_dois)} DOIs to be added to the metadata store.')
+    # Define the keys we're using for our data object:
     good_keys = {'doi': '',
                  'title': [],
                  'subtitle': [],
@@ -72,10 +77,15 @@ def crossref_query(doi_store, metadata_store, verbose = False, check = False, cr
                  'url': '',
                  'valid': True}
     s3 = boto3.client('s3')
-    for doi in checkers:
+    # crossref_dict is a list of processed dict objects pulled from the JSON.
+    # We add to crossref_dict through the following loop:
+    crossref_dict = []
+    for doi in missing_dois:
         try:
             doi_key = 'dois/' + doi + '.json'
             result = s3.get_object(Bucket= metadata_store.get('Bucket'), Key= doi_key)
+            # If the file is not there we'll hop to the exceptions, otherwise we
+            # process the JSON object:
             response = json.loads(result['Body'].read())
             # Convert all keys to lowercase and only keep the "good" keys.
             response_json = {k.lower(): v for k, v in response['message'].items()}
@@ -84,13 +94,19 @@ def crossref_query(doi_store, metadata_store, verbose = False, check = False, cr
             article_dict['valid'] = True
             crossref_dict.append(article_dict)
         except requests.exceptions.RequestException as e:
+            # This generates an object that is effectively empty if we can't find the file:
             article_dict = {k: v for k, v in good_keys.items()}
             article_dict = { 'doi': doi, 'valid': False, 'date': datetime.now() }
             crossref_dict.append(article_dict)
-            warning_msg = f"DOI {doi} not found in CrossRef. Exception: {e}"
-            warnings.warn(warning_msg, category = Warning)
+            if verbose:
+                warning_msg = f"DOI {doi} not found in CrossRef. Exception: {e}"
+                warnings.warn(warning_msg, category = Warning)
         except Exception as e:
-            warnings.warn(e, category = Warning)
+            # If there's some other form of error, let's just skip:
+            if verbose:
+                warnings.warn(e, category = Warning)
+    # Once we're done we turn the dict into a DataFrame and append it to the metadata
+    # DataFrame. We remove any duplicate DOIs.
     cross_ref_df = pd.concat([pd.DataFrame(crossref_dict), metadata],
                              ignore_index = True).drop_duplicates(subset=['doi'])
     push_s3(metadata_store, cross_ref_df, create = True, check = False)
