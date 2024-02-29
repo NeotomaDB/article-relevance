2f83ab023ce13427fabb45dd550c44a6fec8ea0f
SimonGoring <goring@wisc.edu>
2024-02-29 13:41:05 -0800

    Embeddings function cleaned up to run embeddings the single time.
    Had to do some itertools work, using dicts to get the unique list of objects and stuff. All clean now.

--- src/article_relevance/add_embeddings.py
+++ src/article_relevance/add_embeddings.py
@@ -1,16 +1,15 @@
+"""_Generate a dictionary of embeddings for analysis._
+"""
+from datetime import datetime
+from itertools import chain
 import pandas as pd
 from transformers import AutoTokenizer
 from adapters import AutoAdapterModel
-import numpy as np
 from .s3_management import pull_s3, push_s3
-from datetime import datetime
-
-# from .logs import get_logger
-# logger = get_logger(__name__)
 
 def add_embeddings(input_df, text_col,
                    embedding_store,
-                   model_name = 'allenai/specter2_base', 
+                   model_name = 'allenai/specter2_base',
                    adapter_name = 'allenai/specter2_classification',
                    create = True):
     """
@@ -29,24 +28,31 @@ def add_embeddings(input_df, text_col,
                        source="hf",
                        load_as="classification",
                        set_active=True)
-    valid_df = input_df.query("valid")
+    valid_df = input_df.query("valid").to_dict(orient='records')
     try:
-        embedding_object = pull_s3(embedding_store)
-    except:
-        embedding_object = pd.DataFrame({'doi':[], 'date':[]})
-    for i in [i for i in valid_df['doi'] if i not in embedding_object['doi']]:
-        tokens = tokenizer(valid_df.query("doi == @i")[text_col].to_list()[0],
-                              padding='max_length',
-                                  truncation=True,
-                                  max_length=512,
-                                  return_tensors='pt',
-                                  return_token_type_ids=False)
-        output = model(**tokens)
-        embeddings_array = output.last_hidden_state[:, 0, :].detach().numpy()
-        embeddings_df = pd.DataFrame(embeddings_array, columns=[f"embedding_{j}" for j in range(embeddings_array.shape[1])])
-        embed_df = pd.concat([pd.DataFrame({'doi': [i]}), embeddings_df, pd.DataFrame({'date':[datetime.now()]})], axis = 1)
-        embedding_object = pd.concat([embedding_object, embed_df])
-    result = embedding_object.groupby('doi', group_keys=False).apply(lambda group: group.dropna(thresh=group.notna().sum(axis=1).min()))
-    push_s3(embedding_store, result, create = create, check = False)
-    # logger.info(f'Sentence embedding completed.')
+        embedding_object = pull_s3(embedding_store).to_dict(orient='records')
+    except Exception as e:
+        embedding_object = []
+    to_embed = [i for i in valid_df if i.get('doi') not in [j.get('doi') for j in embedding_object]]
+    if len(to_embed) > 0:
+        print(f'Building embeddings for {len(to_embed)} objects.')
+        for i in to_embed:
+            tokens = tokenizer(i.get(text_col),
+                                padding='max_length',
+                                truncation=True,
+                                max_length=512,
+                                return_tensors='pt',
+                                return_token_type_ids=False)
+            output = model(**tokens)
+            embeddings_array = list(chain.from_iterable(output.last_hidden_state[:, 0, :].detach().numpy()))
+            embeddings_key = [f"embedding_{j}" for j in range(len(embeddings_array))]
+            embeddings_dict = dict(zip(embeddings_key, embeddings_array))
+            embeddings_dict['doi'] = i.get('doi')
+            embeddings_dict['date'] = datetime.now()
+            embedding_object = embedding_object + [embeddings_dict]
+        result = pd.DataFrame(embedding_object)
+        push_s3(embedding_store, result, create = create, check = False)
+    else:
+        print('No new objects to be embedded.')
+        result = embedding_object
     return result
