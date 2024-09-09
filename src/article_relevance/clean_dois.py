cfa6e914bd39e6b2634dccdc758eb77b3f72c1e8
Simon <simon.j.goring@gmail.com>
2024-09-09 14:24:26 -0700

    Added doctest testing lines to clean_dois
    First step, but also want to make sure that we are parsing DOIs properly.

--- src/article_relevance/clean_dois.py
+++ src/article_relevance/clean_dois.py
@@ -1,5 +1,10 @@
 import re
 
+"""_clean_dois_
+>>> clean_dois("abcde")
+{"clean": None, "removed": ["abcde"]}
+"""
+
 def clean_dois(dois):
     """_Clean up DOIs by removing whitespace and invalid DOIs_
 
@@ -8,11 +13,43 @@ def clean_dois(dois):
 
     Returns:
         _list_: _A cleaned list of valid DOIs that are strings, with no leading or trailing whitespace._
+    
+    >>> clean_dois(["abcde"])
+    {'clean': [], 'removed': ['abcde']}
+    >>> clean_dois("abcde")
+    {'clean': [], 'removed': ['abcde']}
+    >>> clean_dois(["abcde", "10.1016/j.scitotenv.2023.163947", "https://doi.org/10.1016/j.scitotenv.2023.163947"])
+    {'clean': ['10.1016/j.scitotenv.2023.163947'], 'removed': ['abcde']}
+    >>> clean_dois(["abcde", ["10.1016/j.scitotenv.2023.163947", "https://doi.org/10.1016/j.scitotenv.2023.163947"]])
+    An element of the doi list cannot be parsed as a string and has been removed.
+    Element (<class 'list'>): ['10.1016/j.scitotenv.2023.163947', 'https://doi.org/10.1016/j.scitotenv.2023.163947']
+    If you want to include the record, please fix this data element.
+    {'clean': [], 'removed': ['abcde']}
     """
+    if isinstance(dois, str):
+        dois = [dois]
     if not isinstance(dois, list):
         dois = list(dois)
-    str_dois = [x.strip() for i, x in enumerate(dois) if isinstance(x, str)]
-    regex = re.compile(r'^10\.\d{4,9}/[-.;()/:a-z0-9A-Z]+')
-    clean_dois = [i for i in str_dois if regex.match(i)]
-    bad_dois = [i for i in str_dois if i not in clean_dois]
-    return {'clean': clean_dois, 'removed': bad_dois}
+    regex = re.compile(r'.*(10\.\d{4,9}/[-.;()/:a-z0-9A-Z]+$)')
+    clean_dois = set()
+    bad_dois = set()
+    for i in dois:
+        if not isinstance(i, str):
+            try:
+                bad_dois.add(i)
+            except TypeError as e:
+                    print("An element of the doi list cannot be parsed as a string and has been removed.")
+                    print(f"Element ({type(i)}): {i}")
+                    print("If you want to include the record, please fix this data element.")
+                    continue
+        else:
+            matches = re.search(regex, i)
+            if matches is None:
+                    bad_dois.add(i)
+            else:
+                    clean_dois.add(matches.group(1))
+    return {'clean': list(clean_dois), 'removed': list(bad_dois)}
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
